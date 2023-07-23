0d1ae4ef9a500aa010f5ea931aac8de8bf8ea1bb
beknur <beknurmoldokeev13@gmail.com>
2023-07-24 00:54:01 +0300

    28-beknur

---
+++ script.js
@@ -0,0 +1,10 @@
+let image = document.getElementsByClassName('product-image');
+console.log(image[0]);
+
+let ttl = document.getElementsByClassName('ttl');
+console.log(ttl[0].innerHTML);
+
+let price = document.getElementsByClassName('price');
+
+console.log(price[0].children[0].innerHTML);
+
